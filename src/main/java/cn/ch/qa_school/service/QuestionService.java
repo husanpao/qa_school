38eaee811d5452c2ae7fb1e1cf86e2346af5cd1e
husanpao <1605000001@qq.com>
2022-05-22 12:38:51 +0800

    init

--- src/main/java/cn/ch/qa_school/service/QuestionService.java
+++ src/main/java/cn/ch/qa_school/service/QuestionService.java
@@ -1,7 +1,12 @@
 package cn.ch.qa_school.service;
 
+import cn.hutool.json.JSONObject;
 import org.springframework.stereotype.Service;
 
 @Service
 public class QuestionService {
+    public Object find(JSONObject body) {
+
+        return null;
+    }
 }
